71160880d782bcf08969de3c7a0a48ac4febf75d
ANISH0309 <mittalanish789@gmail.com>
2021-11-18 13:17:18 +0530

    feat: login buttons are fully responsive
    Signed-off-by: ANISH0309 <mittalanish789@gmail.com>

--- src/containers/LoginSection/LoginSection.jsx
+++ src/containers/LoginSection/LoginSection.jsx
@@ -16,22 +16,33 @@ const loginSection = () => {
       <div className="my-48 text-white text-3xl text-center">
         Gallery Incoming
       </div>
-      <div className="flex flex-col justify-center items-center">
+      <div className="flex flex-col items-center">
         <div
           onClick={googleHandler}
-          className="flex w-96 h-14 rounded-md bg-primary mb-6 cursor-pointer"
+          className="flex w-96 xxs:w-80 xxs:px-6 h-14 rounded-md bg-primary mb-6 cursor-pointer"
         >
-          <img className="pl-6 pr-20" src={googleLogo} alt="Google" />
-          <button className="font-400 text-white pr-28" type="submit">
+          <img
+            className="pl-6 pr-20 xxs:pl-4 xxs:pr-10"
+            src={googleLogo}
+            alt="Google"
+          />
+          <button
+            className="font-400 text-base text-white pr-28 xxs:pr-12"
+            type="submit"
+          >
             Sign in with Google
           </button>
         </div>
         <div
           onClick={appleHandler}
-          className="flex w-96 h-14 rounded-md bg-white mb-10 cursor-pointer"
+          className="flex w-96 h-14 xxs:w-80 xxs:px-6 rounded-md bg-white mb-10 cursor-pointer"
         >
-          <img className="pl-6 pr-20" src={appleLogo} alt="Apple" />
-          <button className="font-400" type="submit">
+          <img
+            className="pl-6 pr-20 xxs:pl-4 xxs:pr-10"
+            src={appleLogo}
+            alt="Apple"
+          />
+          <button className="font-400 text-base xxs:pr-12" type="submit">
             Sign in with Apple
           </button>
         </div>
