d1f4f7f13fb180193c3705c8a16b1bf7d1c4374a
ANISH0309 <mittalanish789@gmail.com>
2021-11-23 20:49:39 +0530

    fix: alignment of OAuth buttons
    Signed-off-by: ANISH0309 <mittalanish789@gmail.com>

--- src/containers/LoginSection/LoginSection.jsx
+++ src/containers/LoginSection/LoginSection.jsx
@@ -13,7 +13,7 @@ const loginSection = () => {
   };
 
   return (
-    <div className="">
+    <div className="mx-5">
       <div className="my-48 text-white text-3xl text-center xxs:mx-2">
         Gallery Incoming
       </div>
@@ -22,15 +22,15 @@ const loginSection = () => {
           <div className="flex flex-col items-center justify-center mx-2">
             <div
               onClick={googleHandler}
-              className="flex w-96 xxs:w-80 xxs:px-6 h-14 rounded-md google mb-6 cursor-pointer"
+              className="flex w-96 xs:w-80 xxs:w-72 xxs:px-6 h-14 rounded-md google mb-6 cursor-pointer"
             >
               <img
-                className="pl-6 pr-20 xxs:pl-4 xxs:pr-10"
+                className="pl-6 pr-20 xs:pr-12 xxs:pl-0 xxs:pr-0"
                 src={googleLogo}
                 alt="Google"
               />
               <button
-                className="font-400 text-base text-white pr-28 xxs:pr-12"
+                className="font-400 text-base text-white pl-8 xs:pl-0 xxs:pr-6"
                 type="submit"
               >
                 Sign in with Google
@@ -38,14 +38,17 @@ const loginSection = () => {
             </div>
             <div
               onClick={appleHandler}
-              className="flex w-96 h-14 xxs:w-80 xxs:px-6 rounded-md bg-white mb-10 cursor-pointer"
+              className="flex w-96 h-14 xxs:px-6 xs:w-80 xxs:w-72 rounded-md bg-white mb-10 cursor-pointer"
             >
               <img
-                className="pl-6 pr-20 xxs:pl-4 xxs:pr-10"
+                className="pl-5 pr-20 xs:pr-12 xxs:pl-0 xxs:pr-0"
                 src={appleLogo}
                 alt="Apple"
               />
-              <button className="font-400 text-base xxs:pr-12" type="submit">
+              <button
+                className="font-400 text-base pl-8 xxs:pl-7 xs:pl-0 xxs:pr-12"
+                type="submit"
+              >
                 Sign in with Apple
               </button>
             </div>
