7eff67addcbe54f2bb9547f3f67dbf5f64203ff6
ANISH0309 <mittalanish789@gmail.com>
2021-11-24 16:32:36 +0530

    fix: alignment for smaller screens
    Signed-off-by: ANISH0309 <mittalanish789@gmail.com>

--- src/containers/LoginSection/LoginSection.jsx
+++ src/containers/LoginSection/LoginSection.jsx
@@ -13,46 +13,42 @@ const loginSection = () => {
   };
 
   return (
-    <div className="mx-5">
-      <div className="my-48 text-white text-3xl text-center xxs:mx-2">
+    <div className="relative h-screen mx-5">
+      <div className="pt-28 text-white text-3xl text-center">
         Gallery Incoming
       </div>
-      <div className="flex justify-center items-center">
-        <div className="absolute bottom-10">
-          <div className="flex flex-col items-center justify-center mx-2">
-            <div
-              onClick={googleHandler}
-              className="flex w-96 xs:w-80 xxs:w-72 xxs:px-6 h-14 rounded-md google mb-6 cursor-pointer"
-            >
-              <img
-                className="pl-6 pr-20 xs:pr-12 xxs:pl-0 xxs:pr-0"
-                src={googleLogo}
-                alt="Google"
-              />
-              <button
-                className="font-400 text-base text-white pl-8 xs:pl-0 xxs:pr-6"
-                type="submit"
-              >
-                Sign in with Google
-              </button>
-            </div>
-            <div
-              onClick={appleHandler}
-              className="flex w-96 h-14 xxs:px-6 xs:w-80 xxs:w-72 rounded-md bg-white mb-10 cursor-pointer"
-            >
-              <img
-                className="pl-5 pr-20 xs:pr-12 xxs:pl-0 xxs:pr-0"
-                src={appleLogo}
-                alt="Apple"
-              />
-              <button
-                className="font-400 text-base pl-8 xxs:pl-7 xs:pl-0 xxs:pr-12"
-                type="submit"
-              >
-                Sign in with Apple
-              </button>
-            </div>
-          </div>
+      <div className="absolute bottom-10 flex flex-col items-center justify-center left-0 right-0">
+        <div
+          onClick={googleHandler}
+          className="flex w-96 xs:w-80 xxs:w-full xxs:px-6 h-14 rounded-md google mb-6 cursor-pointer"
+        >
+          <img
+            className="pl-6 pr-20 xs:pr-12 xxs:pl-0 xxs:pr-0"
+            src={googleLogo}
+            alt="Google"
+          />
+          <button
+            className="font-400 text-base text-white pl-8 xs:pl-0 xxs:pl-12"
+            type="submit"
+          >
+            Sign in with Google
+          </button>
+        </div>
+        <div
+          onClick={appleHandler}
+          className="flex w-96 xxs:px-6 xs:w-80 xxs:w-full h-14 rounded-md bg-white mb-6 cursor-pointer"
+        >
+          <img
+            className="pl-5 pr-20 xs:pr-12 xxs:pl-0 xxs:pr-0"
+            src={appleLogo}
+            alt="Apple"
+          />
+          <button
+            className="font-400 text-base pl-8 xxs:pl-10 xs:pl-0 xxs:pr-0"
+            type="submit"
+          >
+            Sign in with Apple
+          </button>
         </div>
       </div>
     </div>
