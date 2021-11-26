b6dfabd511f459ba415cde59c7560fd64629822a
ANISH0309 <mittalanish789@gmail.com>
2021-11-26 18:59:16 +0530

    fix
    Signed-off-by: ANISH0309 <mittalanish789@gmail.com>

--- public/serviceworker.js
+++ public/serviceworker.js
@@ -16,7 +16,7 @@ self.addEventListener("install", (event) => {
   );
 });
 
-// Listen for requests
+// // Listen for requests
 // self.addEventListener("fetch", (event) => {
 //   event.respondWith(
 //     caches.match(event.request).then(() => {
