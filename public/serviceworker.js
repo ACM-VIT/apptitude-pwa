66afad6131e71ef4f1a1ab4d7abab4f3533a2fe5
ANISH0309 <mittalanish789@gmail.com>
2021-11-26 19:05:03 +0530

    fix: serviceworker
    Signed-off-by: ANISH0309 <mittalanish789@gmail.com>

--- public/serviceworker.js
+++ public/serviceworker.js
@@ -16,14 +16,15 @@ self.addEventListener("install", (event) => {
   );
 });
 
-// // Listen for requests
-// self.addEventListener("fetch", (event) => {
-//   event.respondWith(
-//     caches.match(event.request).then(() => {
-//       return fetch(event.request).catch(() => caches.match("offline.html"));
-//     })
-//   );
-// });
+// Listen for requests
+self.addEventListener("fetch", (event) => {
+  event.respondWith(
+    caches.match(event.request).then(() => {
+      return fetch(event.request).catch(() => console.log("Not working"));
+      //   return fetch(event.request).catch(() => caches.match("offline.html"));
+    })
+  );
+});
 
 // Activate the SW
 self.addEventListener("activate", (event) => {
