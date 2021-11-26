a1403d21ec52387e6e13ea951990e5510fd76709
Hemanth Krishna <hkpdev008@gmail.com>
2021-11-26 11:25:24 +0530

    feat: add spinner in otp section

--- src/containers/ProfileSection/ProfileSection.jsx
+++ src/containers/ProfileSection/ProfileSection.jsx
@@ -5,11 +5,14 @@ import axios from "axios";
 import Navbar from "../../components/Navbar/Navbar";
 import copyYellow from "../../assets/images/copy-yellow.svg";
 import { useSnackbar } from 'notistack';
+import LoadingOverlay from 'react-loading-overlay';
 
 const ProfileSection = () => {
   const [copySuccess, setCopySuccess] = useState(false);
   const [data, setData] = useState({});
   const [members, setMembers] = useState([]);
+  const [loading, setloading] = useState(true);
+
   const secret = sessionStorage.getItem("AM");
   const { enqueueSnackbar } = useSnackbar();
 
@@ -21,7 +24,7 @@ const ProfileSection = () => {
       anchorOrigin: {
         vertical: 'top',
         horizontal: 'center',
-    },
+      },
     });
   }
 
@@ -36,6 +39,7 @@ const ProfileSection = () => {
         headers,
       })
       .then((response) => {
+        setloading(false);
         console.log(response.data);
         setData(response.data.data);
         setMembers(response.data.data.members);
@@ -50,39 +54,45 @@ const ProfileSection = () => {
   };
   return (
     <div>
-      <div className="relative h-screen pt-28 mx-5">
-        <div className="xs:flex xs:flex-col sm:flex sm:flex-col">
-          <div className="text-white font-700 text-3xl">Profile Section</div>
-          <div className="text-secondary font-400 text-base mt-9 mb-1">
-            Team name
-          </div>
-        </div>
-        <div className="flex flex-row justify-between ">
-          <div className="text-white font-700 text-2xl">{data.name}</div>
-          <div className=" text-yellow-400 font-700 text-2xl">
-            <CopyToClipboard text={data.code} onCopy={onCopyText}>
-              <div className="flex">
-                {data.code}
-                <img src={copyYellow} alt="copy" className="ml-2.5" />
-              </div>
-            </CopyToClipboard>
-          </div>
-        </div>
-        <div className="flex flex-col mt-20 ">
-          <div className="text-secondary font-400 text-base mb-1">
-            Member {members.length ? "" : "s"}
-          </div>
-          {members.map((member) => (
-            <div
-              key={member.emailId}
-              className="text-white font-700 text-2xl mb-1"
-            >
-              {member.name}
+      <LoadingOverlay
+        active={loading}
+        spinner
+        text='Holup! Looking up who you are'
+      >
+        <div className="relative h-screen pt-28 mx-5">
+          <div className="xs:flex xs:flex-col sm:flex sm:flex-col">
+            <div className="text-white font-700 text-3xl">Profile Section</div>
+            <div className="text-secondary font-400 text-base mt-9 mb-1">
+              Team name
             </div>
-          ))}
+          </div>
+          <div className="flex flex-row justify-between ">
+            <div className="text-white font-700 text-2xl">{data.name}</div>
+            <div className=" text-yellow-400 font-700 text-2xl">
+              <CopyToClipboard text={data.code} onCopy={onCopyText}>
+                <div className="flex">
+                  {data.code}
+                  <img src={copyYellow} alt="copy" className="ml-2.5" />
+                </div>
+              </CopyToClipboard>
+            </div>
+          </div>
+          <div className="flex flex-col mt-20 ">
+            <div className="text-secondary font-400 text-base mb-1">
+              Member {members.length ? "" : "s"}
+            </div>
+            {members.map((member) => (
+              <div
+                key={member.emailId}
+                className="text-white font-700 text-2xl mb-1"
+              >
+                {member.name}
+              </div>
+            ))}
+          </div>
         </div>
-      </div>
-      <Navbar />
+        <Navbar />
+      </LoadingOverlay>
     </div>
   );
 };
--- src/containers/Timeline/Timeline.jsx
+++ src/containers/Timeline/Timeline.jsx
@@ -6,18 +6,20 @@ import {
 import "react-vertical-timeline-component/style.min.css";
 import axios from "axios";
 import Navbar from "../../components/Navbar/Navbar";
+import LoadingOverlay from 'react-loading-overlay';
 
 import "./Timeline.css";
 import Day1 from "../../components/days/day1";
 import Day2 from "../../components/days/day2";
 import Day3 from "../../components/days/day3";
 
-function days() {}
+function days() { }
 
 const Timeline = function () {
   const [data1, setData1] = useState([]);
   const [data2, setData2] = useState([]);
   const [data3, setData3] = useState([]);
+  const [loading, setloading] = useState(true);
 
   const secret = sessionStorage.getItem("AM");
   const headers = {
@@ -39,6 +41,7 @@ const Timeline = function () {
         setData1(timeline.data.day1);
         setData2(timeline.data.day2);
         setData3(timeline.data.day3);
+        setloading(false);
       })
       .catch((error) => console.error(error.response.data));
   }, []);
@@ -49,35 +52,41 @@ const Timeline = function () {
         Timeline
       </div>
       <div className="mb-36">
-        <VerticalTimeline className="vertical-timeline-custom-line">
-          {data1.map((info, key) => (
-            <Day1
-              key={key.id}
-              name={info.name}
-              starttime={info.startTime}
-              endtime={info.endTime}
-              desc={info.description}
-            />
-          ))}
-          {data2.map((info, key) => (
-            <Day2
-              key={key.id}
-              name={info.name}
-              starttime={info.startTime}
-              endtime={info.endTime}
-              desc={info.description}
-            />
-          ))}
-          {data3.map((info, key) => (
-            <Day3
-              key={key.id}
-              name={info.name}
-              starttime={info.startTime}
-              endtime={info.endTime}
-              desc={info.description}
-            />
-          ))}
-        </VerticalTimeline>
+        <LoadingOverlay
+          active={loading}
+          spinner
+          text='Contacting Organisers for the schedule'
+        >
+          <VerticalTimeline className="vertical-timeline-custom-line">
+            {data1.map((info, key) => (
+              <Day1
+                key={key.id}
+                name={info.name}
+                starttime={info.startTime}
+                endtime={info.endTime}
+                desc={info.description}
+              />
+            ))}
+            {data2.map((info, key) => (
+              <Day2
+                key={key.id}
+                name={info.name}
+                starttime={info.startTime}
+                endtime={info.endTime}
+                desc={info.description}
+              />
+            ))}
+            {data3.map((info, key) => (
+              <Day3
+                key={key.id}
+                name={info.name}
+                starttime={info.startTime}
+                endtime={info.endTime}
+                desc={info.description}
+              />
+            ))}
+          </VerticalTimeline>
+        </LoadingOverlay>
       </div>
       <Navbar />
     </div>
