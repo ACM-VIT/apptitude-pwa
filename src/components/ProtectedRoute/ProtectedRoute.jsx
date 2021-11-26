679a8b7c2616c0037194793afb2146735dcb8d71
ANISH0309 <mittalanish789@gmail.com>
2021-11-26 20:23:37 +0530

    fix: validation logic
    Signed-off-by: ANISH0309 <mittalanish789@gmail.com>

--- src/components/ProtectedRoute/ProtectedRoute.jsx
+++ src/components/ProtectedRoute/ProtectedRoute.jsx
@@ -5,14 +5,11 @@ import { Route, Redirect } from "react-router-dom";
 
 const ProtectedRoute = ({ component: Component, redirect, ...rest }) => {
   const key = sessionStorage.getItem("AM");
-  const phone = sessionStorage.getItem("PH");
 
   return (
     <Route
       {...rest}
-      render={(props) =>
-        key && phone ? <Component {...props} /> : <Redirect to="/" />
-      }
+      render={(props) => (key ? <Component {...props} /> : <Redirect to="/" />)}
     />
   );
 };
